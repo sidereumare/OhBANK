367ead0a4f23d22222b79495fca512f2c21c76b5
ByeongHyeon Kim <sidereumare@gmail.com>
2022-10-13 08:54:38 +0900

    파일 추가

---
+++ BackendServer/lib/api/Qna/rewrite.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+var Model = require("../../../models/index");
+var Response = require("../../Response");
+var statusCodes = require("../../statusCodes");
+var { validateUserToken } = require("../../../middlewares/validateToken");
+var { encryptResponse, decryptRequest } = require("../../../middlewares/crypt");
+
