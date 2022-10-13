c3d123c3663e4a966fea22ca73e40e624c6b239d
ByeongHyeon Kim <sidereumare@gmail.com>
2022-10-13 14:53:47 +0900

    기본 구조

--- BackendServer/lib/api/Qna/filedown.js
+++ BackendServer/lib/api/Qna/filedown.js
@@ -6,4 +6,34 @@ var statusCodes = require("../../statusCodes");
 var { validateUserToken } = require("../../../middlewares/validateToken");
 var { encryptResponse, decryptRequest } = require("../../../middlewares/crypt");
 
+/**
+ * QnA file list route
+ * This endpoint allows to view list of files of a question
+ * @path                             - /api/qna/list
+ * @middleware
+ * @param file
+ * @return                           - Qna 
+ */
+
+router.post("/", validateUserToken, (req, res) => {
+    var r = new Response();
+    // test.txt파일 로드
+    var fs = require('fs');
+    var data = fs.readFileSync('test.txt', 'utf8');
+    
+    return res.json(data);
+
+    // let uploadFile = req.files.file;
+    // let uploadPath = req.files.file.name;
+    // uploadFile.mv(uploadPath, function (err) {
+    //     if (err) {
+    //         r.status = statusCodes.SERVER_ERROR;
+    //         r.data = {
+    //             message: err.toString(),
+    //         };
+    //         return res.json(encryptResponse(r));
+    //     }
+    // });
+});
+
 module.exports = router;
