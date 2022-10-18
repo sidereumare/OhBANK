d2d9daa2a4a90b0e38f6096988304b584118b2fa
ByeongHyeon Kim <sidereumare@gmail.com>
2022-10-18 10:08:23 +0900

    filedown vlun

--- BackendServer/lib/api/Qna/filedown.js
+++ BackendServer/lib/api/Qna/filedown.js
@@ -17,46 +17,29 @@ const Readable = require('stream').Readable;
  */
 var fs = require('fs');
 const { response } = require("express");
-var { s3 } = require('../../../middlewares/s3Conn');
 
-router.get("/", validateUserToken, async (req, res) => {
+router.get("/", async (req, res) => {
+    // send to file localfile to client
     var r = new Response();
-    var file_id = decrypt(req.query.file_id);
-    console.log(file_id);
-    Model.file.findOne({
-        where: {
-            id: file_id
-        },
-        attributes: ["file_name", "saved_name"]
-    })
-    .then(async (data) => {
-        var options = {
-            Bucket: 'oh-s3-bucket',
-            Key: data.saved_name
-        };
-
-        var fileStream = s3.getObject(options).createReadStream();
-        res.attachment(data.saved_name);
-        // r.status = statusCodes.SUCCESS;
-        // r.data = data;
-        // res.attachment(data.file_name);
-        // res.attachment(data.saved_name);
-        // file_data = fs.readFileSync("./upload/"+data.saved_name);
-        // s = new Readable();
-        // // s.push(encryptResponse(file_data).enc_data);
-        // s.push(file_data);
-        // s.push(null);
-        // s.pipe(res);
-        fileStream.pipe(res);
-    })
-    .catch((err) => {
+    var filename = req.query.filename;
+    console.log(filename);
+    try{
+        var file = fs.createReadStream(filename);
+        var stat = fs.statSync(filename);
+        res.setHeader('Content-Length', stat.size);
+        res.setHeader('Content-Type', 'application/octet-stream');
+        filename = filename.split('/');
+        filename = filename[filename.length - 1];
+        res.setHeader('Content-Disposition', 'attachment; filename='+filename);
+        file.pipe(res);
+    }
+    catch{
         r.status = statusCodes.SERVER_ERROR;
         r.data = {
-            message: err.toString(),
+            message: "File not found",
         };
-        console.log(r.data);
         return res.json(encryptResponse(r));
-    });
+    }
 });
 
 module.exports = router;
